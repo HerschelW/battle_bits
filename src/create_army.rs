1c7864888f665063364282a3fbc2cb8a38a06d7c
HerschelW <herschelw@gmail.com>
2022-12-19 09:59:49 -0600

    create army functionality now working. Pulling Warrior structure into create_army.rs and passing army vector to main.rs. That took forever

--- src/create_army.rs
+++ src/create_army.rs
@@ -1,42 +1,86 @@
-// define path
-use warrior::Warrior;
+// use std::string::String;
+use std::vec::Vec;
 
-pub struct Army {
-    pub warriors: Vec<Warrior>,
+// import Warrior structure from warrior.rs
+use crate::warrior::Warrior;
+
+// #[derive(Debug)]
+// pub struct Warrior {
+//     // name
+//     pub name: String,
+//     // health
+//     pub health: i32,
+//     // attack modifier
+//     pub attack_modifier: i32,
+//     // defense modifier
+//     pub defense_modifier: i32,
+//     // attack
+//     pub attack: i32,
+//     // defense
+//     pub defense: i32,
+//     // damage
+//     pub damage: i32,
+//     // attack roll
+//     pub attack_roll: i32,
+//     // defense roll
+//     pub defense_roll: i32,
+//     // critial hit
+//     pub critical_hit: bool,
+//     // critical hit damage
+//     pub critical_hit_damage: i32,
+//     // hit
+//     pub hit: bool,
+// }
+
+// pub struct Army {
+//     pub warriors: Vec<Warrior>,
+// }
+
+// pub
+// pub mod test {
+//     let mut army_vector: Vec<Warrior> = Vec::new();
+// }
+
+pub fn army(param: i32) -> Vec<Warrior> {
+    println!("Creating army...");
+
+    // create empty army
+    let mut army_vector: Vec<Warrior> = Vec::new();
+
+    // create 10 warriors
+    for i in 0..param {
+        println!("Creating warrior {}", (i + 1));
+        let warrior = Warrior {
+            name: "Warrior".to_string() + (i + 1).to_string().as_str(),
+            health: 100,
+            attack_modifier: 10,
+            defense_modifier: 10,
+            attack: 0,
+            defense: 0,
+            damage: 0,
+            attack_roll: 0,
+            defense_roll: 0,
+            critical_hit: false,
+            critical_hit_damage: 0,
+            hit: false,
+        };
+        army_vector.push(warrior);
+    }
+    // return army
+    army_vector
 }
 
-impl Army {
-    // create army
-    pub fn create_army(param: i32) {
-        // create empty array of objects
-        let mut warriors: Vec<Warrior> = Vec::new();
-
-        pub fn warrior_profile(params: Vec<i32>, warriors: &mut Vec<Warrior>) {
-            // create warrior
-            let warrior = Warrior {
-                name: "Warrior".to_string() + &params[3].to_string(),
-                health: params[0],
-                attack_modifier: params[1],
-                defense_modifier: params[2],
-                attack: 0,
-                defense: 0,
-                damage: 0,
-                attack_roll: 0,
-                defense_roll: 0,
-                critical_hit: false,
-                critical_hit_damage: 0,
-                hit: false,
-            };
-
-            warriors.push(warrior);
-        }
-
-        // create 10 warriors
-        for i in 0..param {
-            // create warrior
-            warrior_profile(vec![100, 10, 10, i], &mut warriors);
-        }
-
-        warriors;
+pub fn fibonacci(mut val1: i32, mut val2: i32, total: i32) -> Vec<i32> {
+    let mut result = 0;
+    let mut result1 = Vec::new();
+    result1.push(val1);
+    result1.push(result + val2);
+    for _i in 1..(total + 1) {
+        result = val1 + val2;
+        val1 = val2;
+        val2 = result;
+        result1.push(result);
     }
+
+    result1
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 pub mod battle;
 pub mod create_army;
+pub mod warrior;
 
-use battle::battle;
-use create_army::create_army;
+// use battle::battle;
+use create_army::army;
+use create_army::fibonacci;
 fn main() {
+    // start game
+    start_game();
     // battle();
     // create_army(10);
 }
 
 // start game
 pub fn start_game() {
+    println!("Starting game...");
     // create army
-    create_army(10);
-    // battle
-    // battle();
+    // army(10);
     // print army
-    println!("{:?}", create_army::warriors::new())
+    let army = army(10);
+    println!("{:?}", army);
+    // battle
+    // battle(army);
+    // print fibonacci
+    let fib = fibonacci(0, 1, 10);
+    println!("{:?}", fib);
 }
